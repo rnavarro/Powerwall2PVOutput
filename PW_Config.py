7aa1bebb424e6c567d9ffac49ec100f00c328f54
Robert Navarro <rnavarro@stitchlabs.com>
2018-12-07 11:04:07 -0800

    More pep fixes

--- PW_Config.py
+++ PW_Config.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-pvo_host= "pvoutput.org"
+pvo_host = "pvoutput.org"
 pvo_key = ""
 pvo_systemid = ""
 PowerwallIP = ""
 extData = True
 log_file = "pvo.log"
 
-#only needed if using datalogger
+# only needed if using datalogger
 sqlite_file = "pw.sqlite"
-retain_days = "30" #days to retain data in db
+retain_days = "30"  # days to retain data in db
