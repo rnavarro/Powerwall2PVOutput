da3aa411ba12e880dc341bf36b10b93898a298e0
Robert Navarro <rnavarro@stitchlabs.com>
2018-12-07 11:20:35 -0800

    More pep8 changes

--- PW_Datalogger.py
+++ PW_Datalogger.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python
 import time
-import PW_Helper as hlp
-import PW_Config as cfg
 
-hlp.setup_logging(cfg.log_file)
-logger = hlp.logging.getLogger(__name__)
+import PW_Config as Cfg
+import PW_Helper as Hlp
+
+Hlp.setup_logging(Cfg.log_file)
+logger = Hlp.logging.getLogger(__name__)
 logger.info('Start PVOutput datalogger')
 
 while True:
     try:
-    	pw=hlp.getPowerwallData(cfg.PowerwallIP)
-        soc=hlp.getPowerwallSOCData(cfg.PowerwallIP)
-        if (pw!=False and soc!=False):
-            lpvPower=float(pw['solar']['instant_power'])
-            lpvVoltage=float(pw['solar']['instant_average_voltage'])
-            lpvBatteryFlow=float(pw['battery']['instant_power'])
-            lpvLoadPower=float(pw['load']['instant_power'])
-            lpvSitePower=float(pw['site']['instant_power'])
-            lpvLoadVoltage=float(pw['load']['instant_average_voltage'])
-            lpvSOC=float(soc['percentage'])
-            values=(lpvPower,lpvLoadPower,0,lpvVoltage,lpvBatteryFlow,lpvLoadPower,lpvSOC,lpvSitePower,lpvLoadVoltage)
-            hlp.insertdb(cfg.sqlite_file, values)
+        pw = Hlp.getPowerwallData(Cfg.PowerwallIP)
+        soc = Hlp.getPowerwallSOCData(Cfg.PowerwallIP)
+        if pw is not False and soc is not False:
+            lpvPower = float(pw['solar']['instant_power'])
+            lpvVoltage = float(pw['solar']['instant_average_voltage'])
+            lpvBatteryFlow = float(pw['battery']['instant_power'])
+            lpvLoadPower = float(pw['load']['instant_power'])
+            lpvSitePower = float(pw['site']['instant_power'])
+            lpvLoadVoltage = float(pw['load']['instant_average_voltage'])
+            lpvSOC = float(soc['percentage'])
+            values = (
+                lpvPower,
+                lpvLoadPower,
+                0,
+                lpvVoltage,
+                lpvBatteryFlow,
+                lpvLoadPower,
+                lpvSOC,
+                lpvSitePower,
+                lpvLoadVoltage
+            )
+            Hlp.insertdb(Cfg.sqlite_file, values)
         else:
             logger.info('No data received, retrying')
         time.sleep(5)
-        
+
     except StandardError as e:
-        logger.info('Main: Sleeping 5 minutes ' + str(e) )
-        time.sleep(60*5)
+        logger.info('Main: Sleeping 5 minutes ' + str(e))
+        time.sleep(60 * 5)
--- PW_PVOExport.py
+++ PW_PVOExport.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python
 import datetime
-import PW_Helper as hlp
-import PW_Config as cfg
 
-hlp.setup_logging(cfg.log_file)
-logger = hlp.logging.getLogger(__name__)
+import PW_Config as Cfg
+import PW_Helper as Hlp
+
+Hlp.setup_logging(Cfg.log_file)
+logger = Hlp.logging.getLogger(__name__)
 logger.info('Start PVOutput export')
 
 try:
-    pvoutz = hlp.Connection(cfg.pvo_key, cfg.pvo_systemid, cfg.pvo_host)
+    pvoutz = Hlp.Connection(Cfg.pvo_key, Cfg.pvo_systemid, Cfg.pvo_host)
     PVOStatus = pvoutz.get_status()
     pvodate = PVOStatus.split(",")[0]
     pvotime = PVOStatus.split(",")[1]
     sqldate = str(datetime.datetime.strptime(pvodate + " " + pvotime, "%Y%m%d %H:%M"))
-    rows = hlp.get_sqlite_data(cfg.sqlite_file, sqldate)
-    if len(rows)>0:
+    rows = Hlp.get_sqlite_data(Cfg.sqlite_file, sqldate)
+    if len(rows) > 0:
 
         for row in rows:
-            pvPower=row[2]
-            pvVoltage=row[5]
-            pvBatteryFlow=row[6]
-            pvLoadPower=row[7]
-            pvSitePower=row[9]
-            pvLoadVoltage=row[10]
-            pvSOC=row[8]
-            if (pvPower<=30):
-                pvPower=0
-            if (pvLoadPower<0):
-                pvLoadPower=0    
-            pvTemp=row[4]
-            pvConsumption=row[3]
-            if (pvConsumption<0):
-                pvConsumption=0
-            pvDate=row[1]
-            pvTime=row[0]
-            if cfg.extData==True:
-                pvoutz.add_status(pvDate, pvTime, power_exp=pvPower, power_imp=pvConsumption, temp=pvTemp, vdc=pvVoltage, battery_flow=pvBatteryFlow, load_power=pvLoadPower, soc=pvSOC, site_power=pvSitePower, load_voltage=pvLoadVoltage, ext_power_exp=pvPower)
-                std_out="Date: "+str(pvDate)+" Time: " + str(pvTime) + " Watts: " + str(pvPower) + " Load Power: " + str(pvLoadPower) + " SOC: " + str(pvSOC) + " Site Power: " + str(pvSitePower) + " Load Voltage: " + str(pvLoadVoltage) +  " Battery Flow: "+str(pvBatteryFlow)+" Temp: " + str(pvTemp) + " Solar Voltage: " + str(pvVoltage)
+            pvPower = row[2]
+            pvVoltage = row[5]
+            pvBatteryFlow = row[6]
+            pvLoadPower = row[7]
+            pvSitePower = row[9]
+            pvLoadVoltage = row[10]
+            pvSOC = row[8]
+            if pvPower <= 30:
+                pvPower = 0
+            if pvLoadPower < 0:
+                pvLoadPower = 0
+            pvTemp = row[4]
+            pvConsumption = row[3]
+            if pvConsumption < 0:
+                pvConsumption = 0
+            pvDate = row[1]
+            pvTime = row[0]
+            if Cfg.extData is True:
+                pvoutz.add_status(pvDate, pvTime, power_exp=pvPower, power_imp=pvConsumption, temp=pvTemp,
+                                  vdc=pvVoltage, battery_flow=pvBatteryFlow, load_power=pvLoadPower, soc=pvSOC,
+                                  site_power=pvSitePower, load_voltage=pvLoadVoltage, ext_power_exp=pvPower)
+                std_out = "Date: " + str(pvDate) + " Time: " + str(pvTime) + " Watts: " + str(
+                    pvPower) + " Load Power: " + str(pvLoadPower) + " SOC: " + str(pvSOC) + " Site Power: " + str(
+                    pvSitePower) + " Load Voltage: " + str(pvLoadVoltage) + " Battery Flow: " + str(
+                    pvBatteryFlow) + " Temp: " + str(pvTemp) + " Solar Voltage: " + str(pvVoltage)
                 logger.info(std_out)
             else:
-                pvoutz.add_status(pvDate, pvTime, power_exp=pvPower, power_imp=pvConsumption, temp=pvTemp, vdc=pvVoltage)
-                std_out="Date: "+str(pvDate)+" Time: " + str(pvTime) + " Watts: " + str(pvPower) + " Solar Voltage: " + str(pvVoltage)
+                pvoutz.add_status(pvDate, pvTime, power_exp=pvPower, power_imp=pvConsumption, temp=pvTemp,
+                                  vdc=pvVoltage)
+                std_out = "Date: " + str(pvDate) + " Time: " + str(pvTime) + " Watts: " + str(
+                    pvPower) + " Solar Voltage: " + str(pvVoltage)
                 logger.info(std_out)
     else:
-        logger.info("No data returned")               
+        logger.info("No data returned")
 
 except Exception as err:
     logger.info("[ERROR] %s" % err)
 
-#clean up db
-hlp.delete_sqlite_data(cfg.sqlite_file, cfg.retain_days)
+# clean up db
+Hlp.delete_sqlite_data(Cfg.sqlite_file, Cfg.retain_days)
 
 logger.info('End PVOutput export')
